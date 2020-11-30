3f78c4b47eea4b0ce55e896a94499132a0b79186
Jesus Murguia <jesusmurguiafac@gmail.com>
2020-11-30 11:14:29 -0700

    fixed total land

--- api/index.js
+++ api/index.js
@@ -159,13 +159,13 @@ app.get('/biodiversity:combinations', async (req, res) => {
         const { Iteration, scenathon_id, GraficaType } = JSON.parse(req.params.combinations).select;
         switch (GraficaType) {
             case "group":
-                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND(((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 GROUP BY "Country","Year" ORDER BY "Country","Year"';
+                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND((((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100)::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 GROUP BY "Country","Year" ORDER BY "Country","Year"';
                 break;
             case "countries":
-                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND(((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 AND "Country" NOT LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Country","Year" ORDER BY "Country","Year"';
+                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND((((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100)::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 AND "Country" NOT LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Country","Year" ORDER BY "Country","Year"';
                 break;
             case "regions":
-                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND(((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 AND "Country" LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Country","Year" ORDER BY "Country","Year"';
+                var query = 'SELECT "Year","Country", ROUND((avg("CalcBiodivLnd"))::numeric,2) AS "Biodiversity_land",ROUND((((SUM("resultsScen2020"."ProtectedAreasForest" + "resultsScen2020"."ProtectedAreasOtherNat" +"resultsScen2020"."ProtectedAreasOther")) / 12376354)*100)::numeric,2) AS "Protected_land" FROM "resultsScen2020"  WHERE "iteration" = $1 AND "scenathon_id" = $2 AND "Country" LIKE \'%$_%\' ESCAPE \'$\' GROUP BY "Country","Year" ORDER BY "Country","Year"';
                 break;
             default:
                 var query = null;
